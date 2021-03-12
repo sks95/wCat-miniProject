422a5349113472ea0f670d76d7af49595b1ba0e2
SHIVAM <yyyshivamyyy@gmail.com>
2021-03-12 16:47:12 +0530

    modified file readLine

--- Commands/read_file.js
+++ Commands/read_file.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 var path = require('path');
 
-function dispCont(input){
-    for(let i = 0; i < input.length; i++){
-        let fileName = path.basename(input[i]);
+function dispCont(filepath){
+    for(let j = 0; j < filepath.length; j++){
+        // Check if file exists or not
+        let val = fs.existsSync(filepath[j]);
         
-        if (fs.existsSync(fileName)) {
-            //file exists
-            let data = fs.readFileSync(fileName, 'utf8');
+        // If file exists
+        if (val) {
+            let data = fs.readFileSync(filepath[j], 'utf8');
             console.log(data); 
         }
         else {
